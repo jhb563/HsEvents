f4e006a1bfdbcd0490cf6ca02213fb2db207b408
James Bowen <jhbowen047@gmail.com>
2020-01-14 22:09:30 -0800

    Define the different API types

---
+++ common/src/Common/API.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Common.API where
+
+import Data.Int (Int64)
+import Data.Text (Text)
+import Servant.API
+
+import Common.Schema
+
+type UsersApi = "api" :> "users" :>
+  ( "create" :> ReqBody '[JSON] (User, Text) :> Post '[JSON] Int64 :<|>
+    "login" :> ReqBody '[JSON] LoginInfo :> Post '[JSON] Int64
+  )
+
+type EventsApi = "api" :> "events" :>
+  ( BasicAuth "user" NormalUserId :> "browse" :> Get '[JSON] [Event] :<|>
+    BasicAuth "user" NormalUserId :> "purchased" :> Get '[JSON] [(Event, [EventTicket])] :<|>
+    BasicAuth "creator" CreatorUserId :> "create" :> ReqBody '[JSON] EventSummary :> Post '[JSON] Int64 :<|>
+    BasicAuth "creator" CreatorUserId :> "summary" :> Get '[JSON] [(Int64, Event)] :<|>
+    BasicAuth "creator" CreatorUserId :> "summary" :> Capture "event-id" Int64 :> Get '[JSON] EventSummary :<|>
+    BasicAuth "creator" CreatorUserId :> "delete" :> Capture "event-id" Int64 :> Delete '[JSON] ()
+  )
+
+type PurchaseApi = "api" :> "purchases" :>
+  ( BasicAuth "user" NormalUserId :> "enter-queue" :> Capture "event-id" Int64 :> Get '[JSON] () :<|>
+    BasicAuth "user" NormalUserId :> "purchase" :> Capture "event-id" Int64 :> ReqBody '[JSON] [(Text, Int64)] :> Post '[JSON] ()
+  )
+
+type FullApi = UsersApi :<|> EventsApi :<|> PurchaseApi
